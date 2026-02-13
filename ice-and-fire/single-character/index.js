c36e70d081313c914d4b2171d7a19c876976f3b8
Eigentun <winkler2408@gmail.com>
2026-02-13 16:31:43 +0100

    updated promise.all and map function

--- ice-and-fire/single-character/index.js
+++ ice-and-fire/single-character/index.js
@@ -1,34 +1,60 @@
 import fs from "node:fs/promises";
 
+const chars = [
+    "https://anapioficeandfire.com/api/characters/583",
+    "https://anapioficeandfire.com/api/characters/584",
+    "https://anapioficeandfire.com/api/characters/585",
+];
+
 const getData = async (url) => {
-    //try {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
     }
     const result = await response.json();
-    return await result;
-    // } catch (error) {
-    //     console.error(error.message);
-    // }
+    return result;
 };
 
-const writeFile = async (url) => {
-    const character = await getData(url);
-    const characterName = character.name;
-    await fs.writeFile(
-        `character-${characterName}.txt`,
-        characterName,
-    );
-};
+// const getPromises = async (url) => {
+//     const character = getData(url);
+//     return character;
 
-const saveCharacterToFile = async () => {
-    try {
-        await writeFile("https://anapioficeandfire.com/api/characters/583");
-        console.log("success");
-    } catch (error) {
-        console.error(error.message);
+// const characterName = character.name;
+// await fs.writeFile(
+//     `character-${characterName}.txt`,
+//     characterName,
+// );
+// };
+
+// const getPromises = async (url) => {
+//     try {
+//         const promise = await getData(url);
+//         //console.log("success");
+//         return promise;
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// };
+// console.log();
+
+const makePromisesArray = (charArr) => {
+    const promiseArr = [];
+    for (const el of charArr) {
+        promiseArr.push(getData(el));
     }
+
+    //console.log(promiseArr);
+    return promiseArr;
 };
 
-saveCharacterToFile();
+const characters = await Promise.all(makePromisesArray(chars));
+const writeFiles = characters.map((character) => {
+    const characterName = character.name;
+    fs.writeFile(`character-${characterName}.txt`, characterName);
+});
+
+console.log(writeFiles);
+
+//const characterPromises = Promise.all([promises]).then(() => {});
+//console.log(characterPromises);
+//saveCharacterToFile("https://anapioficeandfire.com/api/characters/583");
